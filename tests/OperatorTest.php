30e1e4e0cc7f40ba3d0b1a9c327316f3ba6689ad
hsldymq <hsldymq@qq.com>
2019-01-17 00:53:59 +0800

    Operator单元测试

--- tests/OperatorTest.php
+++ tests/OperatorTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use Archman\ByteOrder\ByteOrder;
+use Archman\ByteOrder\Operator;
 
 class OperatorTest extends TestCase
 {
@@ -12,5 +13,27 @@ class OperatorTest extends TestCase
 
         ByteOrder::set(ByteOrder::BIG_ENDIAN);
         $this->assertEquals(ByteOrder::BIG_ENDIAN, ByteOrder::get());
+    }
+
+    /**
+     * @depends testSetByteOrder
+     */
+    public function testMakeByteArray()
+    {
+        $arr = Operator::toByteArray(0x1001, ByteOrder::LITTLE_ENDIAN);
+        $expect = PHP_INT_SIZE === 8 ? "\x01\x10\x00\x00\x00\x00\x00\x00" : "\x01\x10\x00\x00";
+        $this->assertEquals($expect, $arr);
+
+        $arr = Operator::toByteArray(0x1001, ByteOrder::BIG_ENDIAN);
+        $expect = PHP_INT_SIZE === 8 ? "\x00\x00\x00\x00\x00\x00\x10\x01" : "\x00\x00\x10\x01";
+        $this->assertEquals($expect, $arr);
+    }
+
+    /**
+     * @before
+     */
+    public function resetByteOrder()
+    {
+        ByteOrder::reset();
     }
 }
